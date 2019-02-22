cbdd43b27c7b013b7981ea538e8a92c6e8aec807
MikuAuahDark <auahdark687291@gmail.com>
2019-02-22 23:16:03 +0800

    Allow LuaJIT 2.1 find and make sure to fallback to Lua 5.1 if LuaJIT is not found.

--- cmake/FindLuaJIT.cmake
+++ cmake/FindLuaJIT.cmake
@@ -8,17 +8,19 @@ set(LUAJIT_SEARCH_PATHS
 	/usr/local
 	/usr
 	$ENV{LUAJIT_DIR}
-	)
+)
 
 find_path(LUAJIT_INCLUDE_DIR
-	NAMES luajit.h
-	PATH_SUFFIXES include include/luajit-2.0 src
-	PATHS ${LUAJIT_SEARCH_PATHS})
+	NAMES luajit.h lua.h
+	PATH_SUFFIXES include/luajit-2.1 include/luajit2.1 include/luajit-2.0 include/luajit2.0 include src
+	PATHS ${LUAJIT_SEARCH_PATHS}
+)
 
 find_library(LUAJIT_LIBRARY
-	NAMES luajit-5.1 lua51
+	NAMES luajit5.1 luajit-5.1 lua51
 	PATH_SUFFIXES lib
-	PATHS ${LUAJIT_SEARCH_PATHS})
+	PATHS ${LUAJIT_SEARCH_PATHS}
+)
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(LuaJIT DEFAULT_MSG LUAJIT_LIBRARY LUAJIT_INCLUDE_DIR)
